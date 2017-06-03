6d14ced061f5c7556afb7fb035cdbe1ba523b466
Taylor Ampatiellos <Taylor.Ampatiellos@gmail.com>
2017-06-02 21:12:04 -0400

    Added Moving Balls!
    Added two balls, one controlled with the arrow keys and one with WASD.
    
    The balls have a bouncy material on them that can be edited, as well as
    walking speeds and running speeds that are set using public variables
    (running is toggled by holding shift).

---
+++ Assets/Scripts/MovementP1.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementP1 : MonoBehaviour {
+
+	public int walkspeed;
+	public int runspeed;
+	public Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey("w")) {
+			if (Input.GetKey (KeyCode.LeftShift)) {
+				rb.AddForce (transform.up * runspeed); 
+			} else {
+				rb.AddForce (transform.up * walkspeed);
+			}
+		}
+
+		if (Input.GetKey("a")) {
+			if (Input.GetKey (KeyCode.LeftShift)) {
+				rb.AddForce (Vector2.left * runspeed); 
+			} else {
+				rb.AddForce (Vector2.left * walkspeed);
+			}
+		}
+
+		if (Input.GetKey("s")) {
+			if (Input.GetKey (KeyCode.LeftShift)) {
+				rb.AddForce (Vector2.down * runspeed); 
+			} else {
+				rb.AddForce (Vector2.down * walkspeed);
+			}
+		}
+			
+		if (Input.GetKey ("d")) {
+			if (Input.GetKey (KeyCode.LeftShift)) {
+				rb.AddForce (Vector2.right * runspeed); 
+			} else {
+				rb.AddForce (Vector2.right * walkspeed);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/MovementP2.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementP2 : MonoBehaviour {
+
+	public int walkspeed;
+	public int runspeed;
+	public Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			if (Input.GetKey (KeyCode.RightShift)) {
+				rb.AddForce (transform.up * runspeed); 
+			} else {
+				rb.AddForce (transform.up * walkspeed);
+			}
+		}
+
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			if (Input.GetKey (KeyCode.RightShift)) {
+				rb.AddForce (Vector2.left * runspeed); 
+			} else {
+				rb.AddForce (Vector2.left * walkspeed);
+			}
+		}
+
+		if (Input.GetKey(KeyCode.DownArrow)) {
+			if (Input.GetKey (KeyCode.RightShift)) {
+				rb.AddForce (Vector2.down * runspeed); 
+			} else {
+				rb.AddForce (Vector2.down * walkspeed);
+			}
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			if (Input.GetKey (KeyCode.RightShift)) {
+				rb.AddForce (Vector2.right * runspeed); 
+			} else {
+				rb.AddForce (Vector2.right * walkspeed);
+			}
+		}
+	}
+}
