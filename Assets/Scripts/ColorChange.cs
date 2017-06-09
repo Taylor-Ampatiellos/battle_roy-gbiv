8af1a2c3eb862f8b33f239a0a538d7a0e6693de8
Taylor Ampatiellos <Taylor.Ampatiellos@gmail.com>
2017-06-09 15:10:45 -0400

    Working Prototype!!
    Color switching now works! There are some issues with the edges of the
    sprites but that can be easily remedied in Gimp later on.

---
+++ Assets/Scripts/ColorChange.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour {
+
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+
+		Debug.Log ("HERE");
+
+		if (other.gameObject.name == "Player1") {
+			anim.SetInteger ("Color", 1);
+		}
+
+		if (other.gameObject.name == "Player2") {
+			anim.SetInteger ("Color", 2);
+		}
+	}
+}
