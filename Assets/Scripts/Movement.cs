63bba679fdd48207000319bd0832cfa73a626876
Taylor Ampatiellos <Taylor.Ampatiellos@gmail.com>
2017-06-10 10:45:20 -0400

    Edit to Falling Movement
    Changing the speed variable upon falling works, but needs improvement as
    the ball maintains the speed it flew off the grid with. Need to add code
    to add force in the opposite direction of movement to give the
    impression of slowing down.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Movement : MonoBehaviour {
 
 	public int walkspeed;
-	public int runspeed;
 	private Rigidbody2D rb;
 
 	public string up;
@@ -16,48 +15,35 @@ public class Movement : MonoBehaviour {
 	public float FallSpeed;
 	public Vector2 respawn;
 	private Vector3 originalScale;
+	private int originalSpeed;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
 		originalScale = transform.localScale;
+		originalSpeed = walkspeed;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey(up)) {
-			if (Input.GetKey (KeyCode.LeftShift)) {
-				rb.AddForce (transform.up * runspeed); 
-			} else {
-				rb.AddForce (transform.up * walkspeed);
-			}
+			rb.AddForce (transform.up * walkspeed);
 		}
 
 		if (Input.GetKey(left)) {
-			if (Input.GetKey (KeyCode.LeftShift)) {
-				rb.AddForce (Vector2.left * runspeed); 
-			} else {
-				rb.AddForce (Vector2.left * walkspeed);
-			}
+			rb.AddForce (Vector2.left * walkspeed);
 		}
 
 		if (Input.GetKey(down)) {
-			if (Input.GetKey (KeyCode.LeftShift)) {
-				rb.AddForce (Vector2.down * runspeed); 
-			} else {
-				rb.AddForce (Vector2.down * walkspeed);
-			}
+			rb.AddForce (Vector2.down * walkspeed);
 		}
 			
 		if (Input.GetKey (right)) {
-			if (Input.GetKey (KeyCode.LeftShift)) {
-				rb.AddForce (Vector2.right * runspeed); 
-			} else {
-				rb.AddForce (Vector2.right * walkspeed);
-			}
+			rb.AddForce (Vector2.right * walkspeed);
 		}
 
 		if (transform.localScale.x <= 0) {
+			walkspeed = originalSpeed;
 			transform.position = respawn;
 			transform.localScale = originalScale;
 
@@ -66,6 +52,7 @@ public class Movement : MonoBehaviour {
 
 	void OnTriggerStay2D (Collider2D other) {
 		if (other.gameObject.tag == "Pit" && transform.localScale.x > 0) {
+			walkspeed = 1;
 			transform.localScale -= new Vector3(1, 1, 1) * Time.deltaTime * FallSpeed;
 		}
 	}
