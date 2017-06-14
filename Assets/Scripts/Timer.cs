f55499430f31e0ab15b39fd6e6c4e05979564b72
Taylor Ampatiellos <Taylor.Ampatiellos@gmail.com>
2017-06-14 10:23:19 -0400

    Added a Timer!
    Added a basic timer with a public variable length. Nothing actually
    happens yet when the timer runs out, but it's there for when it's
    needed.

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+	Text number;
+	public int roundLength;
+
+	// Use this for initialization
+	void Start () {
+		number = GetComponent<Text> ();
+		StartCoroutine ("CountDown");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator CountDown () {
+		for (int i = roundLength; i >= 0; i--) {
+			number.text = i.ToString();
+			yield return new WaitForSeconds (1);
+		}
+	}
+}
